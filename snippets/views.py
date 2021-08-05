8b2b1beaa3e78787220925f1fb7dcbb6d69cad52
LongYang <julyzergcn1@gmail.com>
2021-08-05 22:03:24 +0800

    viewset

--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,39 @@
-from django.http import Http404
 from django.contrib.auth.models import User
 
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework import permissions
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 @api_view(['GET'])
